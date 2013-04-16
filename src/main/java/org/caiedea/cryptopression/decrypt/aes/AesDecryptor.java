5eea632424de5ac7f05f159e1b6e0cca4dc3eef9
dshmif <dshmif@gmail.com>
2013-04-16 09:49:21 -0400

    Fully implemented.

--- src/main/java/org/caiedea/cryptopression/decrypt/aes/AesDecryptor.java
+++ src/main/java/org/caiedea/cryptopression/decrypt/aes/AesDecryptor.java
@@ -1,12 +1,19 @@
 package org.caiedea.cryptopression.decrypt.aes;
 
 import org.caiedea.cryptopression.decrypt.Decryptor;
+import org.caiedea.cryptopression.decrypt.DecryptorConfig;
 
 public class AesDecryptor<T> extends Decryptor<T> {
 	protected Decryptor<T> decryptor;
 	
 	public AesDecryptor(Decryptor<T> d) {
+		super();
 		this.decryptor = d;
+	}
+	
+	@Override
+	protected DecryptorConfig configSpecifics() {
+		return null;
 	}
 
 	@Override
