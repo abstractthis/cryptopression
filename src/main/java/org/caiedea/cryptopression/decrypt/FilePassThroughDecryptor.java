b186667561ee3632942f640fac170cb837b42418
dshmif <dshmif@gmail.com>
2013-04-16 09:46:37 -0400

    Fully implemented.

--- src/main/java/org/caiedea/cryptopression/decrypt/FilePassThroughDecryptor.java
+++ src/main/java/org/caiedea/cryptopression/decrypt/FilePassThroughDecryptor.java
@@ -3,10 +3,21 @@ package org.caiedea.cryptopression.decrypt;
 import java.io.File;
 
 public class FilePassThroughDecryptor extends Decryptor<File> {
+	private File targetFile;
+	
+	public FilePassThroughDecryptor(File file) {
+		super();
+		targetFile = file;
+	}
+	
+	@Override
+	protected DecryptorConfig configSpecifics() {
+		return new DecryptorConfig();
+	}
 	
 	@Override
 	public File decrypt() {
-		return null;
+		return targetFile;
 	}
 
 }
