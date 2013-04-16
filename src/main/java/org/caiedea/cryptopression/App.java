dce627ff998bda748c5cc104dc683574802c66c9
dshmif <dshmif@gmail.com>
2013-04-15 20:20:12 -0400

    Implemented test case where the file is compressed before encryption so the compression actually works.

--- src/main/java/org/caiedea/cryptopression/App.java
+++ src/main/java/org/caiedea/cryptopression/App.java
@@ -17,16 +17,16 @@ public class App {
     	String exeDir = System.getProperty("user.dir");
     	System.out.println(exeDir);
     	//System.exit(0);
-    	File file = new File(exeDir + "/encTest.pdf");
+    	File file = new File(exeDir + "/dtl_CD4.xml");
     	File result = new File(exeDir + "/encryptResult");
     	//File resultCompressed = new File(exeDir + "/encryptResult.zip");
     	try {
     		Encryptor<File> noAlgoEncryptor = new FilePassThroughEncryptor(file);
+    		Encryptor<File> compressEncryptor = new CompressFileEncryptor(noAlgoEncryptor);
     		Encryptor<File> fileEncryptor =
-    				new FileAesEncryptor(noAlgoEncryptor, result);
-    		Encryptor<File> compressEncryptor = new CompressFileEncryptor(fileEncryptor);
+    				new FileAesEncryptor(compressEncryptor, result);
     		System.err.println("Performing encryption...");
-    		compressEncryptor.encrypt();
+    		fileEncryptor.encrypt();
     		System.err.println( "encryption DONE!" );
     	}
     	catch(RuntimeException re) {
