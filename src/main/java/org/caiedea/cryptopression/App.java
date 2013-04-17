4e3039a1ca32de13032d8c13c6bc7e401eae566e
dshmif <dshmif@gmail.com>
2013-04-17 01:32:40 -0400

    Implemented full encryption/decryption test.

--- src/main/java/org/caiedea/cryptopression/App.java
+++ src/main/java/org/caiedea/cryptopression/App.java
@@ -2,6 +2,9 @@ package org.caiedea.cryptopression;
 
 import java.io.File;
 
+import org.caiedea.cryptopression.decrypt.Decryptor;
+import org.caiedea.cryptopression.decrypt.FilePassThroughDecryptor;
+import org.caiedea.cryptopression.decrypt.aes.FileAesDecryptor;
 import org.caiedea.cryptopression.encrypt.Encryptor;
 import org.caiedea.cryptopression.encrypt.FilePassThroughEncryptor;
 import org.caiedea.cryptopression.encrypt.aes.FileAesEncryptor;
@@ -19,6 +22,7 @@ public class App {
     	//System.exit(0);
     	File file = new File(exeDir + "/dtl_CD4.xml");
     	File result = new File(exeDir + "/encryptResult");
+    	File decryptedResult = new File(exeDir + "/decrypted.zip");
     	//File resultCompressed = new File(exeDir + "/encryptResult.zip");
     	try {
     		Encryptor<File> noAlgoEncryptor = new FilePassThroughEncryptor(file);
@@ -28,6 +32,11 @@ public class App {
     		System.err.println("Performing encryption...");
     		fileEncryptor.encrypt();
     		System.err.println( "encryption DONE!" );
+    		Decryptor<File> noAlgoDecryptor = new FilePassThroughDecryptor(result);
+    		Decryptor<File> fileDecryptor = new FileAesDecryptor(noAlgoDecryptor, decryptedResult);
+    		System.err.println("Performing decryption...");
+    		fileDecryptor.decrypt();
+    		System.err.println("decryption DONE!!!");
     	}
     	catch(RuntimeException re) {
     		System.err.println(re);
